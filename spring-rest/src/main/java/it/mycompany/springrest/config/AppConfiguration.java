90d01c64995f78a69f462d47f332c1b4e6d5b34a
S.DellAria-cons <S.DellAria-cons@AC-SDELLARIA.itmaster.local>
2021-05-24 12:37:11 +0200

    Added spring-rest project
    Implemented all Java configuration and first Rest Service.

---
+++ spring-rest/src/main/java/it/mycompany/springrest/config/AppConfiguration.java
@@ -0,0 +1,12 @@
+package it.mycompany.springrest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("it.mycompany.springrest")
+public class AppConfiguration {
+
+}
---
+++ spring-rest/src/main/java/it/mycompany/springrest/config/DispatcherConfiguration.java
@@ -0,0 +1,23 @@
+package it.mycompany.springrest.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherConfiguration extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { AppConfiguration.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
