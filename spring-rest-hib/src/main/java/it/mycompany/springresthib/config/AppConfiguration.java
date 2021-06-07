705bf5bc654c54a8913c05105a3dece6a95dff82
S.DellAria-cons <S.DellAria-cons@AC-SDELLARIA.mshome.net>
2021-06-07 16:05:25 +0200

    Solved Error on Transaction Management.
    Added Missing Annotation inside AppConfiguration class.

--- spring-rest-hib/src/main/java/it/mycompany/springresthib/config/AppConfiguration.java
+++ spring-rest-hib/src/main/java/it/mycompany/springresthib/config/AppConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,6 +22,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 @ComponentScan(basePackages = "it.mycompany.springresthib")
 @PropertySource("classpath:persistence-mysql.properties")
 public class AppConfiguration implements WebMvcConfigurer{
